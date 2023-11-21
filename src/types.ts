bd139417c77c2a0bfe4cd57a6b22c7f6b4255621
Renato <renatomercadoluna@hotmail.com>
2023-11-21 15:37:07 -0500

    first commit

---
+++ src/types.ts
@@ -0,0 +1,33 @@
+export interface ContactItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface CompatibilityItem {
+  icon: string;
+  title: string;
+  url: string;
+}
+
+export interface RPGBenefit {
+  title: string;
+  description: string;
+}
+
+export interface FeatureItem {
+  description: string;
+  icon: string;
+  title: string;
+}
+
+export interface FooterLink {
+  description: string;
+  icon: string;
+  url: string;
+}
+
+export interface NavItem {
+  title: string;
+  url: string;
+}
