120195451f27b91301f5d84fa443d8fc68dc1d62
MarcoHuamanOllero <marco.huaman.ollero@gmail.com>
2024-05-29 10:48:38 -0500

    Se agregaron los aliados

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rpgdevs.com",
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    react(),
+  ],
   vite: {
     ssr: {
       external: ["svgo"],
