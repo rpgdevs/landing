26ff0cc00036b5f0e8eb225eea84df979b8424e7
MarcoHuamanOllero <marco.huaman.ollero@gmail.com>
2024-06-18 17:34:52 -0500

    ultimas correciones

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,16 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import astroI18next from "astro-i18next";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.rpgdevs.com",
   base: "",
-  integrations: [
-    tailwind(),
-    react(),
-    astroI18next(),
-    icon()
-  ],
+  integrations: [tailwind(), react(), astroI18next(), icon(), svelte()],
   i18n: {
     defaultLocale: 'es',
-    locales: [ 'es', 'en' ],
+    locales: ['es', 'en'],
     routing: {
       prefixDefaultLocale: false
     },
@@ -26,7 +23,7 @@ export default defineConfig({
   },
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
