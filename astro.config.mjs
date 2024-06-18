89737c99ddd465900fc960857247c38be455d3a7
MarcoHuamanOllero <marco.huaman.ollero@gmail.com>
2024-06-18 15:37:12 -0500

    Partners, i18n, Splash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,7 +10,19 @@ export default defineConfig({
   integrations: [
     tailwind(),
     react(),
+    astroI18next(),
+    icon()
   ],
+  i18n: {
+    defaultLocale: 'es',
+    locales: [ 'es', 'en' ],
+    routing: {
+      prefixDefaultLocale: false
+    },
+    fallback: {
+      en: 'es'
+    }
+  },
   vite: {
     ssr: {
       external: ["svgo"],
---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const languages = {
+    es: 'Español',
+    en: 'English',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    es: {
+      'nav.home':'Inicio',
+      'nav.intro':'Intro',
+      'nav.services':'Servicios', 
+      'nav.compatibility':'¿Por que RPG?',
+      'nav.partners':'Aliados'
+    },
+    en: {
+      'nav.home':'Home',
+      'nav.intro':'Intro',
+      'nav.services':'Services', 
+      'nav.compatibility':'Why RPG?',
+      'nav.partners':'Partners'
+    },
+  } as const;
