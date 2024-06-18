d6e97822a89aa66394b6fd34d309528ce41fd019
MarcoHuamanOllero <marco.huaman.ollero@gmail.com>
2024-06-18 16:28:19 -0500

    www

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rpgdevs.com",
+  site: "https://www.rpgdevs.com",
   base: "",
   integrations: [
     tailwind(),
