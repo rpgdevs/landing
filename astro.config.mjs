848d46535cf2409bda14958906f650d24abc3e2b
MarcoHuamanOllero <marco.huaman.ollero@gmail.com>
2024-06-20 17:23:57 -0500

    ReCAPTCHA

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   site: "https://www.rpgdevs.com",
   base: "",
   integrations: [tailwind(), react(), astroI18next(), icon(), svelte()],
-  output:"server",
+  output: 'server',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
---
+++ src/components/recaptcha.js
@@ -0,0 +1,22 @@
+export async function POST({ request }) {
+    const data = await request.json();
+  
+    const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify';
+    const requestHeaders = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    };
+    const requestBody = new URLSearchParams({
+      secret: "COLOCAR CLAVE SECRETA",   // Esto puede ser una variable de entorno
+      response: data.recaptcha          // El token pasado desde el cliente
+    });
+  
+    const response = await fetch(recaptchaURL, {
+      method: "POST",
+      headers: requestHeaders,
+      body: requestBody.toString()
+    });
+  
+    const responseData = await response.json();
+  
+    return new Response(JSON.stringify(responseData), { status: 200 });
+  }
